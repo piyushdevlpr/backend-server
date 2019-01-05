799a9690c6a31d8261d0ea02334a5158362ee008
piyushdevlpr <piyush80108@gmail.com>
2019-01-05 18:25:02 +0530

    backend

---
+++ data.js
@@ -0,0 +1,28 @@
+ var ob = [
+  {
+    id : "2911",
+    events : ["1929 - On 31 December 1929, Congress passed the historic resolution declaring Purna Swaraj as its goal.",
+        "1922-Birth of Swaraj Party - Chittaranjan Das - President - Motilal Nehru - Secretary",
+    "1600-East India Company was given the Royal charter"]  
+  },
+  {
+    id : "3111",
+    events : ["1929 - On 31 December 1929, Congress passed the historic resolution declaring Purna Swaraj as its goal.",
+        "1922-Birth of Swaraj Party - Chittaranjan Das - President - Motilal Nehru - Secretary",
+    "1600-East India Company was given the Royal charter"]  
+  },
+  {
+    id : "3011", 
+    events : ["In the year 1877, Queen Victoria of the erstwhile British Empire is proclaimed the Empress of India.",
+    "During the 1949 war with Pakistan – Prime Minister approached United Nations against the armed insurgency by Pakistan. According to an UN decree, a cease-fire took effect on this date and the war between the two countries stopped and a status-quo was maintained thereafter."]
+  },{
+    id : "0200", 
+    events : ["A 1975 bomb blast at Samastipur, Bihar fatally wounded the then Minister of Railways, Lalit Narayan Mishra. The sessions court gave a verdict in a trial after 39 years which convicted 5 accused of secretly plotting and murdering the minister and 3 others. The verdict was later quashed by the high court and all of accused were acquitted.",
+    "Mannathu Padnabha Pillai – The social reformer and freedom fighter from the state of Kerala was born in 1878 in the erstwhile princely state of Travancore. He dedicated his life to uplift the status of the Nair community and thus founded NSS (Nair Service Society).",
+    "Kirti Azad – The former cricketer is the son of former Bihar chief minister, Bhagwat Jha Azad. Azad remained active in cricket between 1980 to 86 and was part of the squad which won 1983 Cricket world cup. He’s also a Member of Parliament from Darbhanga, Bihar."]
+  },{
+    id : "0300", 
+    events : ["Bharatiya Janata Party (BJP) leader Jaswant Singh who served as minister of finance, defence and external affairs in the union cabinet was born on January 3, 1938 at Jasol village in Rajasthan’s Barmer district to Sardara Singh and Kunwar Baisa.",
+    ]
+  }]
+  module.exports=ob ;
---
+++ index.js
@@ -0,0 +1,20 @@
+var express     = require("express"),
+    app         = express(),
+    server= require("http").createServer(app),
+    ob = require('./data');
+//var obj = "hello";
+
+app.get("/",function(req,res){
+  var d = new Date();
+  for(let i in ob){
+   // console.log(ob[i]["id"]) ;
+    if(ob[i]["id"] === d.getDate()+""+d.getMonth()){
+      res.json(ob[i]["events"]) ;
+    }
+  }
+    
+});
+var port = process.env.PORT || 3000;
+server.listen(port,function(){ 
+  console.log("server running!!!!");
+});
