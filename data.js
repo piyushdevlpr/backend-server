eb5fb583fe2a22859e64e3b091dc430f0826a439
piyushdevlpr <piyush80108@gmail.com>
2019-01-05 22:12:00 +0530

    bac

--- data.js
+++ data.js
@@ -16,17 +16,17 @@
     events : ["In the year 1877, Queen Victoria of the erstwhile British Empire is proclaimed the Empress of India.",
     "During the 1949 war with Pakistan – Prime Minister approached United Nations against the armed insurgency by Pakistan. According to an UN decree, a cease-fire took effect on this date and the war between the two countries stopped and a status-quo was maintained thereafter."]
   },{
-    id : "0200", 
+    id : "20", 
     events : ["A 1975 bomb blast at Samastipur, Bihar fatally wounded the then Minister of Railways, Lalit Narayan Mishra. The sessions court gave a verdict in a trial after 39 years which convicted 5 accused of secretly plotting and murdering the minister and 3 others. The verdict was later quashed by the high court and all of accused were acquitted.",
     "Mannathu Padnabha Pillai – The social reformer and freedom fighter from the state of Kerala was born in 1878 in the erstwhile princely state of Travancore. He dedicated his life to uplift the status of the Nair community and thus founded NSS (Nair Service Society).",
     "Kirti Azad – The former cricketer is the son of former Bihar chief minister, Bhagwat Jha Azad. Azad remained active in cricket between 1980 to 86 and was part of the squad which won 1983 Cricket world cup. He’s also a Member of Parliament from Darbhanga, Bihar."]
   },{
-    id : "0500", 
+    id : "50", 
     events : ["Bharatiya Janata Party (BJP) leader Jaswant Singh who served as minister of finance, defence and external affairs in the union cabinet was born on January 3, 1938 at Jasol village in Rajasthan’s Barmer district to Sardara Singh and Kunwar Baisa.",
     ]
   },
   {
-    id : "0600", 
+    id : "60", 
     events : ["1066 – Edward the Confessor dies childless, sparking a succession crisis that will eventually lead to the Norman conquest of England.",
       "1477 – Battle of Nancy: Charles the Bold is killed and Burgundy becomes part of France.",
       "1500 – Duke Ludovico Sforza conquers Milan.",
@@ -40,5 +40,5 @@
       "1875 – The Palais Garnier, one of the most famous opera houses in the world, is inaugurated in Paris.",
       "1882 – Charles J. Guiteau is found guilty of assassinating US President James A. Garfield, and is sentenced to death by hanging."
    ]
-  }]
+  }];
   module.exports=ob ;
--- index.js
+++ index.js
@@ -1,86 +1,13 @@
 var express     = require("express"),
     app         = express(),
-    server= require("http").createServer(app);
-  var ob = [
-      {
-        id : "2911",
-        events : ["1929 - On 31 December 1929, Congress passed the historic resolution declaring Purna Swaraj as its goal.",
-            "1922-Birth of Swaraj Party - Chittaranjan Das - President - Motilal Nehru - Secretary",
-        "1600-East India Company was given the Royal charter"]  
-      },
-      {
-        id : "3111",
-        events : ["1929 - On 31 December 1929, Congress passed the historic resolution declaring Purna Swaraj as its goal.",
-            "1922-Birth of Swaraj Party - Chittaranjan Das - President - Motilal Nehru - Secretary",
-        "1600-East India Company was given the Royal charter"]  
-      },
-      {
-        id : "3011", 
-        events : ["In the year 1877, Queen Victoria of the erstwhile British Empire is proclaimed the Empress of India.",
-        "During the 1949 war with Pakistan – Prime Minister approached United Nations against the armed insurgency by Pakistan. According to an UN decree, a cease-fire took effect on this date and the war between the two countries stopped and a status-quo was maintained thereafter."]
-      },{
-        id : "0200", 
-        events : ["A 1975 bomb blast at Samastipur, Bihar fatally wounded the then Minister of Railways, Lalit Narayan Mishra. The sessions court gave a verdict in a trial after 39 years which convicted 5 accused of secretly plotting and murdering the minister and 3 others. The verdict was later quashed by the high court and all of accused were acquitted.",
-        "Mannathu Padnabha Pillai – The social reformer and freedom fighter from the state of Kerala was born in 1878 in the erstwhile princely state of Travancore. He dedicated his life to uplift the status of the Nair community and thus founded NSS (Nair Service Society).",
-        "Kirti Azad – The former cricketer is the son of former Bihar chief minister, Bhagwat Jha Azad. Azad remained active in cricket between 1980 to 86 and was part of the squad which won 1983 Cricket world cup. He’s also a Member of Parliament from Darbhanga, Bihar."]
-      },{
-        id : "0500", 
-        events : ["Bharatiya Janata Party (BJP) leader Jaswant Singh who served as minister of finance, defence and external affairs in the union cabinet was born on January 3, 1938 at Jasol village in Rajasthan’s Barmer district to Sardara Singh and Kunwar Baisa.",
-        ]
-      },
-      {
-        id : "0600", 
-        events : ["1066 – Edward the Confessor dies childless, sparking a succession crisis that will eventually lead to the Norman conquest of England.",
-          "1477 – Battle of Nancy: Charles the Bold is killed and Burgundy becomes part of France.",
-          "1500 – Duke Ludovico Sforza conquers Milan.",
-          "1527 – Felix Manz, a leader of the Anabaptist congregation in Zürich, Switzerland, is executed by drowning.",
-          "1554 – A great fire occurs in Eindhoven, Netherlands.",
-          "1664 – Maratha forces under Chhatrapati Shivaji defeat the Mughals in the Battle of Surat.",
-          "1675 – Battle of Colmar: The French army beats Brandenburg.",
-          "1757 – Louis XV of France survives an assassination attempt by Robert-François Damiens, the last person to be executed in France by drawing and quartering, the traditional and gruesome form of capital punishment used for regicides.",
-          "1781 – American Revolutionary War: Richmond, Virginia, is burned by British naval forces led by Benedict Arnold.",
-          "1846 – The United States House of Representatives votes to stop sharing the Oregon Territory with the United Kingdom.",
-          "1875 – The Palais Garnier, one of the most famous opera houses in the world, is inaugurated in Paris.",
-          "1882 – Charles J. Guiteau is found guilty of assassinating US President James A. Garfield, and is sentenced to death by hanging.",
-          "1895 – Dreyfus affair: French army officer Alfred Dreyfus is stripped of his rank and sentenced to life imprisonment on Devil's Island.",
-          "1911 – Kappa Alpha Psi, the world's third oldest and largest black fraternity, is founded at Indiana University.",
-          "1912 – The Prague Party Conference takes place.",
-          "1913 – First Balkan War: During the Battle of Lemnos, Greek admiral Pavlos Kountouriotis forces the Turkish fleet to retreat to its base within the Dardanelles, from which it did not venture for the rest of the war.",
-          "1914 – The Ford Motor Company announces an eight-hour workday and minimum daily wage of $5 in salary plus bonuses[1] subject to restrictions and imposed character standards.",
-          "1919 – The German Workers' Party, which would become the Nazi Party, is founded.",
-          "1925 – Nellie Tayloe Ross of Wyoming becomes the first female governor in the United States.",
-          "1933 – Construction of the Golden Gate Bridge begins in San Francisco Bay.",
-          "1944 – The Daily Mail becomes the first transoceanic newspaper.",
-          "1945 – The Soviet Union recognizes the new pro-Soviet government of Poland.",
-          "1949 – United States President Harry S. Truman unveils his Fair Deal program.",
-          "1950 – In the Sverdlovsk air disaster, all 19 of those on board were killed, including almost the entire national ice hockey team (VVS Moscow) of the Soviet Air Force – 11 players, as well as a team doctor and a masseur.",
-          "1953 – The play Waiting for Godot by Samuel Beckett is first performed.",
-          "1957 – In a speech given to the United States Congress, United States President Dwight D. Eisenhower announces the establishment of what will later be called the Eisenhower Doctrine.",
-          "1968 – Alexander Dubček comes to power; Prague Spring begins in Czechoslovakia.",
-          "1970 – The 7.1 Mw  Tonghai earthquake shakes Tonghai County, Yunnan province, China, with a maximum Mercalli intensity of X (Extreme). Between 10,000–15,621 were killed and 26,783 were injured.",
-          "1972 – United States President Richard Nixon orders the development of a Space Shuttle program.",
-          "1974 – Warmest reliably measured temperature below the Antarctic Circle of +59 °F (+15 °C) recorded at Vanda Station",
-          "1975 – The Tasman Bridge in Tasmania, Australia, is struck by the bulk ore carrier Lake Illawarra, killing twelve people.",
-          "1976 – The Khmer Rouge proclaim the Constitution of Democratic Kampuchea.",
-          "1976 – The Troubles: Gunmen shoot dead ten Protestant civilians after stopping their minibus at Kingsmill in County Armagh, Northern Ireland, UK, allegedly as retaliation for a string of attacks on Catholic civilians in the area by Loyalists, particularly the killing of six Catholics the night before.",
-          "1991 – Georgian forces enter Tskhinvali, the capital of South Ossetia, Georgia, opening the 1991–92 South Ossetia War.",
-          "1991 – The United States Embassy to Somalia in Mogadishu is evacuated by helicopter airlift days after violence enveloped Mogadishu during the Somali Civil War",
-          "1993 – The oil tanker MV Braer runs aground on the coast of the Shetland Islands, spilling 84,700 tons of crude oil.",
-          "2000 – Sri Lankan Civil War: Sri Lankan Tamil politician Kumar Ponnambalam is shot dead in Colombo.",
-          "2005 – Eris, the most massive and second-largest known dwarf planet in the Solar System, is discovered by the team of Michael E. Brown, Chad Trujillo, and David L. Rabinowitz using images originally taken on October 21, 2003, at the Palomar Observatory.",
-          "2014 – A launch of the communication satellite GSAT-14 aboard the GSLV MK.II D5 marks the first successful flight of an Indian cryogenic engine."
-        ]
-      }]
-//var obj = "hello";
+    server = require("http").createServer(app),
+    ob = require('./data');
 
-app.get("/",function(req,res){
+app.get("/data",function(req,res){
   var d = new Date();
   for(let i in ob){
-   // console.log(ob[i]["id"]) ;
     if(ob[i]["id"] === d.getDate()+""+d.getMonth()){
-      res.send(ob[i]["events"]) ;
-    }else{
-      res.send("hello");
+      res.json(ob[i]["events"]) ;
     }
   }
  // ob[i]["events"]
